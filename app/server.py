68b39eecd3e538d0f9c2e2f703cd15b023fffdba
RanDan363 <38443741+RanDan363@users.noreply.github.com>
2019-11-12 14:19:16 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1slSOyLRfjqUscqBQh3jTEb6czLz09uUT1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1slSOyLRfjqUscqBQh3jTEb6czLz09uUT'
 export_file_name = 'export.pkl'
 
 classes = ['apples', 'cherrys', 'tomatoes']
